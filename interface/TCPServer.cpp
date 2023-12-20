ef14ea137fd12a44677cb7eed6c8c9fb8214ba18
Markel Sainz <msainz@ikerlan.es>
2023-12-20 16:01:21 +0100

    New version. It build. Ready to try.

--- interface/TCPServer.cpp
+++ interface/TCPServer.cpp
@@ -38,39 +38,3 @@ void TCPServer::client_desconnected(int socket_fd)
     }
 
 }
-
-template <class S>
-void TCPServer::run() 
-{
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        throw SocketFailedException();
-
-    // Setting sock options
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &TCPServer::OPTED, sizeof(TCPServer::OPTED)))
-        throw SetSockOptFailedException();
-    
-    address.sin_family = AF_INET;  
-    address.sin_addr.s_addr = INADDR_ANY;  
-    address.sin_port = htons(TCPServer::PORT);
-
-    addrlen = sizeof(address);
-
-    // Forcefully attaching socket to port 8080
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
-        throw BindFailedException();
-
-    if (listen(server_fd, 3) < 0)
-        throw ListenFailedException();
-    
-    while(!done) {
-        sem.acquire();
-        if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0)
-            throw AcceptFailedException();
-        iTCPAgent *ta = new S(new_socket, this); 
-        std::thread new_client {&iTCPAgent::run, ta};
-        tcp_clients.insert({new_socket, std::make_pair(std::move(ta), std::move(new_client))});
-
-        //Any max client limitation strategy goes here
-    }
-}
--- interface/TCPServer.h
+++ interface/TCPServer.h
@@ -7,19 +7,11 @@
 #include <string>
 #include <thread>
 #include "iTCPAgent.h"
+#include "TCP_exceptions.h"
 #include <semaphore>
 
 class TCPServer
 {
-public:
-    TCPServer();
-    TCPServer(TCPServer&& ts);
-    ~TCPServer();
-
-    template <class S>
-    void run();
-    void client_desconnected(int socket_fd);
-
 private:
     int server_fd;
     int new_socket;
@@ -33,6 +25,49 @@ private:
     static const int PORT;
     static const int OPTED;
     static const int MAX_CLIENTS;
+
+public:
+    TCPServer();
+    TCPServer(TCPServer&& ts);
+    ~TCPServer();
+
+    void client_desconnected(int socket_fd);
+
+    template <class S>
+    void run()
+    {
+        // Creating socket file descriptor 
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+            throw SocketFailedException();
+
+        // Setting sock options
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &TCPServer::OPTED, sizeof(TCPServer::OPTED)))
+            throw SetSockOptFailedException();
+    
+        address.sin_family = AF_INET;  
+        address.sin_addr.s_addr = INADDR_ANY;  
+        address.sin_port = htons(TCPServer::PORT);
+
+        addrlen = sizeof(address);
+
+        // Forcefully attaching socket to port 8080
+        if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
+            throw BindFailedException();
+
+        if (listen(server_fd, 3) < 0)
+            throw ListenFailedException();
+        
+        while(!done) {
+            sem.acquire();
+            if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0)
+                throw AcceptFailedException();
+            iTCPAgent *ta = new S(new_socket, this); 
+            std::thread new_client {&iTCPAgent::run, ta};
+            tcp_clients.insert({new_socket, std::make_pair(std::move(ta), std::move(new_client))});
+
+            //Any max client limitation strategy goes here
+        }
+    }
 };
 
 #endif
